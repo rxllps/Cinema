0a1238fa3efc659b0f1c5b1ece6cd6f89db19a7e
rxllps <linoxek2005@gmail.com>
2026-05-31 02:31:23 +0300

    234

--- karaoke/urls.py
+++ karaoke/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("admin/",              admin.site.urls),
-    path("",                    include("rooms.urls")),
-    path("accounts/",           include("django.contrib.auth.urls")),
-    path("otziv/",              include("otziv.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("rooms.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("otziv/", include("otziv.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
